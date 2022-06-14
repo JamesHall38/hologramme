0ccdf3964d9fe12b836b8aa582239acb1825bcaf
JamesHall38 <95545311+JamesHall38@users.noreply.github.com>
2022-06-14 08:01:09 +0200

    test

--- src/Model/Utils/Resources.js
+++ src/Model/Utils/Resources.js
@@ -50,12 +50,13 @@ export default class Resources extends EventEmitter {
     // }
 
     addGLTF(arrayBuffer) {
-        // console.log(arrayBuffer)
-        // this.loaders.gltfLoader.parse(
-        //     arrayBuffer, '', (file) => {
-        //         this.importedLoaded(file)
-        //     }
-        // )
+        console.log(arrayBuffer)
+        this.loaders.gltfLoader.parse(
+            arrayBuffer, '', (file) => {
+                console.log(file)
+                // this.importedLoaded(file)
+            }
+        )
     }
 
     addOBJ(arrayBuffer) {
