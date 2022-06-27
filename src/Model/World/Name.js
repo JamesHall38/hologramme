776c2b849f003491d23fa87007efa2e0b8dd6dcf
JamesHall38 <95545311+JamesHall38@users.noreply.github.com>
2022-06-27 17:16:14 +0200

    fix z-fighting

--- src/Model/World/Name.js
+++ src/Model/World/Name.js
@@ -74,10 +74,9 @@ export default class Name {
             this.ctx.fillStyle = "white"
             this.ctx.font = "75px sans-serif"
 
-            this.cube.position.set(0.05, this.pos, 0.518)
+            this.cube.position.set(0.05, this.pos, 0.52)
             this.cube.rotation.y = Math.PI
 
-            console.log(this.animationName)
             this.ctx.fillText(this.animationName, 50, 100)
             this.texture.needsUpdate = true
 
